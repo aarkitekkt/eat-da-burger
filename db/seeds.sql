3c7fb6da0bd878819e6bf4fe162a456bcd4d8eaf
aarkitekkt <johnson.brycej@gmail.com>
2020-02-23 12:30:23 -0700

    set up file structure

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheddar Burger", false), ("Big Mac", false), ("Whopper", false);
---
+++ models/burger.js
@@ -0,0 +1,5 @@
+var orm = require("../config/orm.js");
+
+// Code to call the orm functions using burger specific input for the orm.
+
+module.exports = burger;
