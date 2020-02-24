4cecc7184525f426b909ec778196505d1845a7db
aarkitekkt <johnson.brycej@gmail.com>
2020-02-23 17:42:50 -0700

    create update and get routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,11 +6,33 @@ var router = express.Router();
 
 router.get("/", function (req, res) {
     burger.all(function (data) {
-        var hbsObject = {
+        var burgersData = {
             burgers: data
         };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+        console.log(burgersData);
+        res.render("index", burgersData);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        // res.json({ id: result.insertId });
+        console.log(req.body.burger_name + " has been added to the burger list!");
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var burgerId = "Burger id = " + req.params.id;
+
+    console.log(burgerId);
+
+    burger.update(true, req.params.id, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
--- server.js
+++ server.js
@@ -12,15 +12,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 app.use(routes);
 
-
 app.listen(PORT, function () {
-
     console.log("Server listening on: http://localhost:" + PORT);
 });
